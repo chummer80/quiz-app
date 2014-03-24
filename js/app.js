dd815d4aaf6d756d4686b0569fc0ca7a9049834f
chummer80 <mike.c.chu@gmail.com>
2014-03-23 23:26:12 -0700

    beginning game logic

--- js/app.js
+++ js/app.js
@@ -3,10 +3,24 @@ $(document).ready(function () {
 	*	CONSTANTS
 	************************/
 	
+	
+	/************************
+	*	GAME VARIABLES
+	************************/
+	
+	var questionNum = 0;
+	var numCorrectAnswers = 0;
+	
+	var questionPanel = $('#question_panel');
+	var answerPanel = $('#answer_panel');
+	
+	var questionPanelQuizText = questionPanel.find($('.quiz_text'))
+	var answerPanelQuizText = answerPanel.find($('.quiz_text'))
 
 	/************************
 	*	QUIZ QUESTIONS
 	************************/
+	
 	var quizQuestions = [];
 	
 	quizQuestions.push(new quizQuestion(
@@ -32,11 +46,20 @@ $(document).ready(function () {
 		2,
 		"As a member of the Philadelphia Warriors, Wilt Chamberlain scored 100 points against the New York Knicks on March 2, 1962."
 	));
-
 	
-	/************************
-		EVENT HANDLERS
-	************************/
+	quizQuestions.push(new quizQuestion(
+		"In what year did the NBA merge with the American Basketball Association (ABA)?",
+		[
+			"1966",
+			"1969",
+			"1972",
+			"1976"
+		],
+		3,
+		"The NBA and ABA merged in 1976. This added 4 new teams to the NBA: Denver Nuggets, Indiana Pacers, New Jersey Nets, and San Antonio Spurs."
+	));
+
+
 	
 
 		
@@ -44,16 +67,74 @@ $(document).ready(function () {
 		HELPER FUNCTIONS
 	************************/
 	
+	// Fill in all the text of the next question on the question panel
+	var populateQuestionPanel = function populateQuestionPanel(questionNum, questionObject) {
+		
+		questionPanelQuizText.find('#question_number').text(questionNum + ". ");
+
+		// changing the text in the text node without destroying the span is a little harder
+		// first get all text nodes in the #question_text paragraph element
+		var textNodes = questionPanelQuizText.find('#question_text').contents().filter(function() {
+			return this.nodeType == Node.TEXT_NODE;
+		});
+		// for some reason there are 2 text nodes. We only want the 2nd one.
+		var textNode = textNodes.get(1);
+		textNode.nodeValue = questionObject.getQuestion();
+		
+		// clear the choices from the previous question
+		var choiceListElement = questionPanelQuizText.find('#choice_list');
+		choiceListElement.children().remove();
+		
+		var choices = questionObject.getChoices();
+		for (var i = 0; i < choices.length; i++) {
+			$('<li></li>')
+				.addClass('quiz_choice')
+				.text(choices[i])
+				.appendTo(choiceListElement);
+		}
+	};
 	
+	// reset game
+	var resetQuiz = function resetQuiz() {
+		// reset visuals
+		questionPanelQuizText.hide();
+		questionPanel.removeClass('next_panel');
+		questionPanel.addClass('this_panel');
+				
+		answerPanelQuizText.hide();
+		answerPanel.removeClass('this_panel');
+		answerPanel.addClass('next_panel');
+		
+		// reset game data
+		questionNum = 0;
+		numCorrectAnswers = 0;
+	};
 	
+	// start new game
+	var startQuiz = function startQuiz() {
+		questionNum = 1;
+		populateQuestionPanel(questionNum, quizQuestions[questionNum-1]);
+		
+		//questionPanelQuizText.fadeIn(2000);
+		questionPanelQuizText.show();
+		
+	};
+		
+		
+	/************************
+		EVENT HANDLERS
+	************************/
 	
-	console.log(quizQuestions[0].getQuestion());
-	console.log(quizQuestions[0].getChoices());
-	console.log(quizQuestions[0].getAnswer());
-	console.log(quizQuestions[0].getExtraInfo());
+	$('.quiz_choice').click(function() {
+		var choiceIndex = this.index();
+	});
 	
-	console.log(quizQuestions[1].getQuestion());
-	console.log(quizQuestions[1].getChoices());
-	console.log(quizQuestions[1].getAnswer());
-	console.log(quizQuestions[1].getExtraInfo());
+	/************************
+	*	START 
+	************************/
+	
+
+	// populateQuestionPanel(0, quizQuestions[0
+	// resetQuiz();
+	startQuiz();
 });

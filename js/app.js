b7f7d628f2574241f5648ebc54441e205fcc51ef
chummer80 <mike.c.chu@gmail.com>
2014-03-24 05:53:12 -0700

    Put quiz question definitions back in the main js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,69 @@
 $(document).ready(function () {
+	
 	/************************
-	*	CONSTANTS
+	*	QUIZ QUESTIONS
 	************************/
-	
-	
+		
+	var quizQuestions = [];
+
+	quizQuestions.push(new quizQuestion(
+		"Who was the inventor of the game of basketball?",
+		[
+			"James Naismith",
+			"Larry O'Brien",
+			"John Wooden",
+			"David Stern"
+		],
+		0,
+		"James Naismith, a Canadian-American sports coach and innovator, invented the sport of basketball in 1891."
+	));
+
+	quizQuestions.push(new quizQuestion(
+		"What is the record for the most points scored by a single player in an NBA game?",
+		[
+			"81 points",
+			"90 points",
+			"100 points",
+			"103 points"
+		],
+		2,
+		"As a member of the Philadelphia Warriors, Wilt Chamberlain scored 100 points against the New York Knicks on March 2, 1962."
+	));
+
+	quizQuestions.push(new quizQuestion(
+		"In what year did the NBA merge with the American Basketball Association (ABA)?",
+		[
+			"1966",
+			"1969",
+			"1972",
+			"1976"
+		],
+		3,
+		"The NBA and ABA merged in 1976. This added 4 new teams to the NBA: the Denver Nuggets, the Indiana Pacers, the New Jersey Nets, and the San Antonio Spurs."
+	));
+
+	quizQuestions.push(new quizQuestion(
+		"Which NBA team has won the most championships?",
+		[
+			"The Los Angeles Lakers",
+			"The Chicago Bulls",
+			"The San Antonio Spurs",
+			"The Boston Celtics"
+		],
+		3,
+		"The Boston Celtics have won 17 championships, the most of any team in the NBA. From 1959-1966 they won 8 in a row!"
+	));
+
 	/************************
 	*	GAME VARIABLES
 	************************/
 	
 	var questionNum = 0;
 	var numCorrectAnswers = 0;
+	
+	/************************
+	*	DOM ELEMENTS
+	************************/
 	
 	var questionPanel = $('#question_panel');
 	var answerPanel = $('#answer_panel');
